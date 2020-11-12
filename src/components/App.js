69ea9bd4bdb04d258fed7f2f557288c53c100a48
BMatillaVaras <bmatillavaras@gmail.com>
2020-11-12 08:57:54 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../stylesheets/App.scss";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="App">Hola mundo</div>;
+  }
+}
+
+export default App;
